06f838ca20131446bef54fbf6ad1f94a30f6fafa
freedomxuanyuan <freedomxuanyuan@gmail.com>
2023-01-05 20:39:49 +0800

    init repo

---
+++ src/main/java/top/freedom/freecommunity/FreecommunityApplication.java
@@ -0,0 +1,13 @@
+package top.freedom.freecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreecommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreecommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/freedom/freecommunity/FreecommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package top.freedom.freecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreecommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
